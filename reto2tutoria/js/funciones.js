88a01caa7bceee475976a9247c6d16c95f431b9c
L Angela G <luangu2020@gmail.com>
2022-09-22 07:42:40 -0400

    cambios agregar boton para eliminar

--- reto2tutoria/js/funciones.js
+++ reto2tutoria/js/funciones.js
@@ -2,44 +2,133 @@
 const api = "https://g02ae6c99368ff0-room.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client";
 
 class Hotel {
-  static traerCliente() {
-  $.ajax(api, {
-    type: "GET", 
-    contentType: "application/json",
-    dataType: "JSON",
-    crossDomain: true,
-    success: function(respuesta){
-    console.log(respuesta);
-    },
-    error: function(xhr, error){
-        console.log(error)
-    },
-  })
-}
-//funcion post
-static crearCliente() {
-    let datos = {
-        id:$("#id").val(),
-        name:$("#name").val(),
-        email:$("#email").val(),
-        age:$("#age").val(),
-    };
-let dataToSend=JSON.stringify(datos);
+    static traerCliente() {
+        $.ajax(api, {
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            crossDomain: true,
+            success: function (respuesta) {
+                console.log(respuesta);
+                // $("#resultadoCliente").append(respuesta.items[0].name);
+                $("#resultadoCliente").html("");
+                for (let i = 0; i < respuesta.items.length; i++) {
+                    let fila = `<tr>
+                        <td>${respuesta.items[i].id}</td>
+                        <td>${respuesta.items[i].name}</td>
+                        <td>${respuesta.items[i].email}</td>
+                        <td>${respuesta.items[i].age}</td>
+                        <td><button onclick="Hotel.eliminarCliente()">Eliminar</button></td>
+                    </tr>`;
+                    $("#resultadoCliente").append(fila);
 
-$.ajax(api, {
-    type:"POST",
-    data:data,
-    dataType:"json",
-    success:function(respuesta){
-        id:$("#id").val();
-        name:$("#name").val();
-        email:$("#email").val();
-        age:$("#age").val();
-        get();
-        alert("Guardado")
+                }
+            },
+            error: function (xhr, error) {
+                console.log(error)
+            },
+        })
     }
-})
+    //funcion post
+    static crearCliente() {
+        console.log("entro a la funcion")
+        let datos = {
+            id: $("#id").val(),
+            name: $("#name").val(),
+            email: $("#email").val(),
+            age: $("#age").val(),
+        };
+        let dataToSend = JSON.stringify(datos);
+        console.log(dataToSend);
+        $.ajax({
+            url: "https://g02ae6c99368ff0-room.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client",
+            type: "POST",
+            data: dataToSend,
+            dataType: "json",
+            contentType: "application/json",
 
-}
+            complete: function (respuesta) {
+                if (respuesta.status == 201) {
+                    Hotel.traerCliente();
+                    $("#id").val("");
+                    $("#name").val("");
+                    $("#email").val("");
+                    $("#age").val("");
+                    alert("Guardado")
+                } else {
+                    alert("Error, no se puedo agregar")
+                }
+            }
+
+        })
+    }
+
+    //funcion put
+    static actualizarCliente() {
+        console.log("entro a la funcion")
+        let datos = {
+            id: $("#id").val(),
+            name: $("#name").val(),
+            email: $("#email").val(),
+            age: $("#age").val(),
+        };
+        let dataToSend = JSON.stringify(datos);
+        console.log(dataToSend);
+        $.ajax({
+            url: "https://g02ae6c99368ff0-room.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client",
+            type: "PUT",
+            data: dataToSend,
+            dataType: "json",
+            contentType: "application/json",
+
+            complete: function (respuesta) {
+                if (respuesta.status == 201) {
+                    Hotel.traerCliente();
+                    $("#id").val("");
+                    $("#name").val("");
+                    $("#email").val("");
+                    $("#age").val("");
+                    alert("Actualizado")
+                } else {
+                    alert("Error, no se puedo actualizar")
+                }
+            }
+
+        })
+    }
+
+    //funcion delete
+    static eliminarCliente() {
+        console.log("entro a la funcion")
+        let datos = {
+            id: $("#id").val(),
+            // name: $("#name").val(),
+            //email: $("#email").val(),
+            ///age: $("#age").val(),
+        };
+        let dataToSend = JSON.stringify(datos);
+        console.log(dataToSend);
+        $.ajax({
+            url: "https://g02ae6c99368ff0-room.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client",
+            type: "DELETE",
+            data: dataToSend,
+            dataType: "json",
+            contentType: "application/json",
+
+            complete: function (respuesta) {
+                if (respuesta.status == 204) {
+                    Hotel.traerCliente();
+                    $("#id").val("");
+                    $("#name").val("");
+                    $("#email").val("");
+                    $("#age").val("");
+                    alert("Eliminado")
+                } else {
+                    alert("Error, no se puedo eliminar")
+                }
+            }
+
+        })
+    }
 }
 
